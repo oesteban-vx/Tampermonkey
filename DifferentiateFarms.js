671521fc2bd92584516473f7e7994f0f1f5f593f
Oscar Esteban <oscar.esteban@vexcelgroup.com>
2021-07-14 08:56:21 +0200

    Use a lighter orange

--- DifferentiateFarms.js
+++ DifferentiateFarms.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Differentiate farms
 // @namespace    http://tampermonkey.net/
-// @version      0.4
+// @version      0.5
 // @description  Add some differentiation to farms!
 // @author       oesteban
 // @updateURL    https://raw.githubusercontent.com/oesteban-vx/Tampermonkey/main/DifferentiateFarms.js
@@ -22,7 +22,7 @@ const stringHashCode = str => {
 }
 
 var farm_abbrevs = {
-    "publishing" : ["pub", "eec694"], // orange
+    "publishing" : ["pub", "ffe6c8"], // orange
     "reproj"     : ["rep", "b7c6f4"], // blue
     "reproj2"    : ["rep2", "c7f9f4"],
     "reproj3"    : ["rep3", "8bfbf0"],
